e75718a0b11e0cae6985ff13c83d72f7a06e21ff
geekskick <geeks_kick@yahoo.co.uk>
2022-11-26 12:17:30 +0000

    Raw capture rate shown

---
+++ src/capture_gauge.js
@@ -0,0 +1,64 @@
+import GaugeChart from "react-gauge-chart";
+import React from 'react';
+
+
+export class CaptureGuage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            max_level: 255
+        };
+    }
+
+    toPercentage(rate) {
+        const rc = rate / this.state.max_level;
+        console.log("Capture Rate " + rate + " as a percentage is " + rc);
+        return rc;
+    }
+
+    fromPercentage(rate) {
+        const rc = parseInt(this.state.max_level * (rate / 100), 10);
+        console.log("Percentage " + rate + " as a rate is " + rc);
+        return rc;
+    }
+
+    render() {
+        const chartStyle = {
+            height: 250,
+        }
+        let gauge;
+        console.log("rendering CaptureGuage");
+
+        if (this.props.rate) {
+            gauge = <GaugeChart
+                style={chartStyle}
+                nrOfLevels={this.state.max_level / 10}
+                percent={this.toPercentage(this.props.rate)}
+                formatTextValue={value => this.fromPercentage(value)}
+                colors={["#ff0000", "#00ff00"]}
+                animDelay={0}
+                animateDuration={1000}
+                textColor="#000000"
+                arcWidth={0.3} />;
+        }
+        else {
+            gauge = <GaugeChart
+                style={chartStyle}
+                nrOfLevels={this.state.max_level / 10}
+                animDelay={0}
+                percent={0.50}
+                formatTextValue={value => "No rate available"}
+                colors={["#808080"]}
+                animateDuration={1000}
+                textColor="#000000"
+                arcWidth={0.3} />;
+        }
+        return (
+            <div>
+                {/*<p>{this.props.mon}</p>*/}
+                {gauge}
+            </div>)
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CaptureGuage  } from './capture_gauge';
+import { PokemonList } from './pokemon_list';
+import Pokedex from 'pokedex-promise-v2';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const P = new Pokedex();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            selected_mon: null,
+            capture_rate: null
+        }
+    }
+
+    async onSelect(mon) {
+        console.log("onSelect::mon = ", mon);
+        try {
+            const prom = await this.props.api.getPokemonSpeciesByName(mon);
+            console.log("onSelect::prom = ", prom);
+            this.setState({
+                selected_mon: mon,
+                capture_rate: prom.capture_rate
+            });
+        }
+        catch (error) {
+            console.log("onSelect::error = ", error);
+            this.setState({
+                capture_rate: null,
+                selected_mon: mon
+            });
+        }
+    }
+
+    render() {
+        return (<div style={{width:250}}><PokemonList names={this.props.names} on_select={this.onSelect.bind(this)} />
+            <CaptureGuage mon={this.state.selected_mon} rate={this.state.capture_rate}/>
+        </div>)
+    }
+
+}
+
+async function getPokemonList(api) {
+    console.log("getPokemonList::api = ", api);
+    const prom = await api.getPokemonsList({ limit: 1500 });
+    console.log("getPokemonList::promise = ", prom);
+    console.log("getPokemonList::prom = ", prom);
+    console.log("getPokemonList:: returning");
+    return prom.results.map(p => p.name);
+}
+
+
+// ========================================
+
+(async () => {
+    try {
+        const names = await getPokemonList(P);
+        console.log("names = ", names);
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(<App api={P} names={names} />);
+    } catch (error) {
+        console.log("Error = ", error);
+        throw error;
+    }
+})()
