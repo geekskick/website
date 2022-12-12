fa8dbbc85588e38aa0de12560fbf9ccc727c6fa0
geekskick <geeks_kick@yahoo.co.uk>
2022-12-12 20:30:57 +0000

    Index is simpler

--- src/index.js
+++ src/index.js
@@ -2,45 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Pokedex from 'pokedex-promise-v2';
-import LabelledDropdown from './labelled_dropdown';
 import 'react-dropdown/style.css';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 import { ErrorBoundary } from './error_boundary';
 import PokemonListManager from './pokemon_list_manager';
-import calculateCaptureRate from './calculations'
-import FilteringSpriteGallery from './filtering_sprite_gallery'
-import getWindowDimensions from './window_dimensions';
 import NavBar from './navbar';
 import AboutDialog from './about_dialog';
-import FilteringAutocomplete from './filtering_autocomplete';
 import Balls from './data/balls.json'
 import Grid from '@mui/material/Grid'
 import LinearProgress from '@mui/material/LinearProgress';
-import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import Typography from '@mui/material/Typography';
+import Autocomplete from '@mui/material/Autocomplete';
 import _ from "lodash";
 
 
 function App(props) {
     console.log("App props = ", props);
     const [selectedPokemon, setSelectedPokemon] = React.useState('');
-    const [captureRate, setCaptureRate] = React.useState(0.0);
     const [selectedGeneration, setSelectedGeneration] = React.useState('');
-    const [captureProbability, setCaptureProbability] = React.useState(0.0);
     const [selectedBall, setSelectedBall] = React.useState('');
-    const [windowDimensions, setWindowDimensions] = React.useState(getWindowDimensions());
     const [aboutOpen, setAboutOpen] = React.useState(false);
-    const [filteredPokemonOptions, setFilteredPokemonOptions] = React.useState([]);
-    const [filteredBallOptions, setFilteredBallOptions] = React.useState([]);
-    // 1151 here is the max number of pokemon, I need to pass this into the function via props
-    const shinyIdx = React.useRef(getRandomInt(1151));
-    const [results, setResults] = React.useState({});
-
-    function handleWindowResize() {
-        setWindowDimensions(getWindowDimensions());
-    }
+    const [selectedStatusAilment, setSelectedStatusAilment] = React.useState('');
+    const [captureProbability, setCaptureProbability] = React.useState({ possible: false, probability: 0.0 });
 
     const onGenericError = (error, info) => {
         NotificationManager.error(error + info);
@@ -54,8 +38,8 @@ function App(props) {
 
     const onSelectPokemon = (mon) => {
         console.log(`onSelect::mon = ${mon}`);
-        setSelectedPokemon(mon.target.value);
-        NotificationManager.info(`Selected ${mon.target.value}`);
+        setSelectedPokemon(mon);
+        NotificationManager.info(`Selected ${mon}`);
     }
 
     const onSelectGenerations = (mon) => {
@@ -67,19 +51,6 @@ function App(props) {
     const onStatusChange = (status) => {
         Notification.info(`New status = ${status}`);
     }
-
-    /**
-     * In the pokemonlist I want to make a random pokemon as shiny,
-     * this is determined by the Reference 'shinyIdx' contents. 
-     * There is an attribute in the pokemon to determin if it's shingy 
-     * or not and simply sets it to true
-     */
-    const setRandomPokemonToShiny = () => {
-        if (props.pokemonList.length >= shinyIdx.current) {
-            props.pokemonList[shinyIdx.current].shiny = true;
-        }
-    }
-
 
     /**
      * The about window uses a single state variable to indicate if it's open or not.
@@ -97,50 +68,6 @@ function App(props) {
         setAboutOpen(false);
     }
 
-    /**
-     * The list sent to the SpriteGallery is expected to contain objects with the attributes,
-     * { name, sprite {url, shiny}, id, tooltip }
-     * so we need to change the shape of the data in the pokemonlist to fit. This method takes a single 
-     * pokemon and returns a new object with the data in the right place for that component
-     * @param {*} pokemon a pokemon
-     * @returns object with data in expected format for the sprite gallery
-     */
-    const massagePokemonToFitIntoThePokemonImageList = (pokemon) => {
-        return {
-            name: pokemon.name,
-            sprite: {
-                url: pokemon.shiny ? pokemon.data?.sprites.front_shiny : pokemon.data?.sprites.front_default,
-                shiny: pokemon.shiny
-            },
-            id: pokemon.data?.id,
-            tooltip: pokemon.species?.flavor_text_entries.filter(entry => {
-                return entry.language.name === "en"
-            })[0].flavor_text.replace('\f', '\n')
-        }
-    }
-
-    /**
-     * Calculates the sprite based on the screen width. Currently
-     * this is a proportion of the screen (20%), or 164 pixels. Whichever is largest
-     * @param {Number} screenWidth The screen width in pixels
-     * @returns 
-     */
-    const calculateSpriteWidth = (screenWidth) => {
-        return Math.max(0.2 * screenWidth, 164);
-    }
-
-    setRandomPokemonToShiny();
-
-    // TODO Remove all this shit and make the image dimensions as a percentage of the screen using css or something like that?
-    const screenWidth = windowDimensions.width;
-    const spriteWidth = calculateSpriteWidth(screenWidth);
-    const rows = screenWidth / spriteWidth;
-
-    console.log("With a width of ", screenWidth, " and a sprite dimension of ", spriteWidth, " I can fit ", rows, " in");
-    console.log("Selected Pokemon = " + selectedPokemon);
-    console.log("Ball list = ", props.ballList);
-    console.log("Pokemonlist = ", props.pokemonList);
-
     const mainData = (() => {
         if (props.dataLoading) {
             return <div>
@@ -156,55 +83,51 @@ function App(props) {
                 */}
                     <Grid container spacing={2}>
                         <Grid item xs={4}>
-                            <LabelledDropdown
+                            <Autocomplete
+                                autoComplete
                                 options={props.pokemonList.map(pokemon => pokemon.name)}
-                                onChange={onSelectPokemon}
-                                label="Select a Pokemon"
-                                placeholder="Select a Pokemon"
-                                value={selectedPokemon}
-                            />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <LabelledDropdown
-                                options={props.generations}
-                                onChange={(event) => {
-                                    console.log(event);
-                                    onSelectGenerations(event.target.value);
+                                onChange={(event, value, reason) => {
+                                    console.log(event, value, reason);
+                                    if (reason === "selectOption") {
+                                        onSelectPokemon(value);
+                                    }
                                 }}
-                                placeholder="Select a Generation"
-                                label="Select a Generation"
-                                value={selectedGeneration}
+                                isOptionEqualToValue={(left, right) => { return left.name === right.name; }}
+                                value={selectedPokemon}
+                                renderInput={(params) => <TextField {...params} label="Select a Pokemon" />}
                             />
                         </Grid>
                         <Grid item xs={4}>
                             <Autocomplete
                                 autoComplete
-                                isOptionEqualToValue={_.isEqual}
-                                options={props.statusOptions}
-                                onChange={(_, newValue) => {
-                                    onStatusChange(newValue.id);
+                                options={props.generations}
+                                onChange={(event, value, reason) => {
+                                    console.log(event, value, reason);
+                                    if (reason === "selectOption") {
+                                        onSelectGenerations(value);
+                                    }
                                 }}
-                                renderInput={(params) => <TextField {...params} label={"Select a Status Ailment"} />}
+                                value={selectedGeneration}
+                                renderInput={(params) => <TextField {...params} label="Select a Generation" />}
+                            />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Autocomplete
+                                autoComplete
+                                options={props.statusOptions}
+                                onChange={(event, value, reason) => {
+                                    console.log(event, value, reason);
+                                    if (reason === "selectOption") {
+                                        onStatusChange(value);
+                                    }
+                                }}
+                                value={selectedStatusAilment}
+                                renderInput={(params) => <TextField {...params} label="Select a Status Ailment" />}
                             />
                         </Grid>
                     </Grid>
                     <hr className="rule" />
-                    {/*<FilteringSpriteGallery
-                        superSetSpriteList={props.pokemonList.filter(pokemon => {
-                            const listExists = filteredPokemonOptions.length > 0;
-                            if (!listExists) {
-                                // If there's no filter list then we want to show the lot
-                                return true;
-                            }
-                            return filteredPokemonOptions.includes(pokemon.name);
-                        }).map(pokemon => massagePokemonToFitIntoThePokemonImageList(pokemon))}
-                        dimension={spriteWidth}
-                        columns={rows}
-                        rows={3}
-                    />*/}
-                    <hr className="rule" />
-                    <hr className="rule" />
-                    <h1 className="probability">{captureProbability}</h1>
+                    <h1 className="probability">{captureProbability.probability}</h1>
                 </ErrorBoundary>);
         }
     })();
@@ -276,7 +199,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
         root.render(<AppView />);
     } catch (error) {
         console.log("Error = ", error);
-        NotificationManager.error("Unable to get either the pokemon list, or the generation list. Try refreshing the page");
-        root.render(<NotificationContainer />);
     }
 })()
