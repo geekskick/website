955c8658ddc7fc780094a7b4934b5f757389b3f7
geekskick <geeks_kick@yahoo.co.uk>
2022-12-16 08:09:56 +0000

    removed unused caching

--- src/result_card.js
+++ src/result_card.js
@@ -3,10 +3,8 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { CardHeader } from '@mui/material';
-import { getOrUseCachedPokemonData, getOrUseCachedSpeciesData } from './get_or_use_cached';
+import { CardHeader, Tooltip } from '@mui/material';
 
 
 
@@ -16,61 +14,44 @@ export default function ResultCard(props) {
     const [pokemonDetails, setPokemonDetails] = React.useState();
     const [speciesDetails, setSpeciesDetails] = React.useState();
 
-    const pokemonSpeciesLoader = async (pokemon, callback) => {
-        try {
-            const species = await props.api.getPokemonSpeciesByName(pokemon.split("_")[0]);
-            console.log("Details for ", pokemon, species);
-            callback(pokemon, species);
-        }
-        catch (error) {
-            console.log(error);
-            callback(pokemon, null);
-        }
-
-    }
-
-    const pokemonDetailsLoader = async (pokemon, callback) => {
-        try {
-            const species = await props.api.getPokemonByName(pokemon.split("_")[0]);
-            console.log("Details for ", pokemon, species);
-            callback(pokemon, species);
-        }
-        catch (error) {
-            console.log(error);
-            callback(pokemon, null);
-        }
-    }
-
     React.useEffect(() => {
         console.log("ResultCard::useEffect");
-        getOrUseCachedPokemonData(props.selectedPokemon, pokemonDetailsLoader, (data) => {
-            console.log("Got ", data);
-            setPokemonDetails(data);
+
+        props.api.getPokemonByName(props.selectedPokemon).then(pokemon => {
+            setPokemonDetails(pokemon);
+        }).catch(err => {
+            console.log(err);
+            setPokemonDetails(null);
         });
-        getOrUseCachedSpeciesData(props.selectedPokemon, pokemonSpeciesLoader, (data) => { setSpeciesDetails(data) });
+
+        props.api.getPokemonSpeciesByName(props.selectedPokemon).then(pokemon => {
+            setSpeciesDetails(pokemon);
+        }).catch(err => {
+            console.log(err);
+            setSpeciesDetails(null);
+        });
+
     }, [props.selectedPokemon]);
 
     let sprite;
     let generationSprites;
+    let tooltip;
     if (pokemonDetails) {
         generationSprites = Object.entries(pokemonDetails?.sprites.versions[props.selectedGeneration]);
     }
     if (generationSprites) {
         sprite = generationSprites[0][1].front_default;
+        tooltip = `${props.selectedGeneration} ${props.selectedPokemon}`
         if (!sprite) {
             console.log("There are NO generation sprites for this (" + props.selectedPokemon + "," + props.selectedGeneration + "), so using default");
             sprite = pokemonDetails?.sprites.front_default;
+            tooltip = `There are no ${props.selectedGeneration} sprites for ${props.selectedPokemon} so this is just the default one`
         }
     }
     if (!sprite) {
         sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+        tooltip = `There are no sprite available for ${props.selectedPokemon}, so this is a default one`
     }
-
-    console.log("ResultCard props = ", props);
-    console.log("SpeciesData = ", speciesDetails);
-    console.log("PokemonData = ", pokemonDetails);
-    console.log("Generation sprites = ", generationSprites);
-    console.log("Selected sprite = ", sprite);
 
     return (<Card sx={{
         width: '100', display: 'flex'
@@ -78,14 +59,15 @@ export default function ResultCard(props) {
         <CardHeader
             title={props.selectedPokemon}
         />
-        <CardMedia
-            component="img"
-            height={100}
-            image={sprite}
-            alt={props.selectedPokemon}
-            sx={{ width: 100 }}
-
-        />
+        <Tooltip title={tooltip}>
+            <CardMedia
+                component="img"
+                height={100}
+                image={sprite}
+                alt={props.selectedPokemon}
+                sx={{ width: 100 }}
+            />
+        </Tooltip>
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
             <CardContent sx={{ flex: '1 0 auto' }}>
                 <Typography>{props.selectedPokemon}</Typography>
