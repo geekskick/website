a59d70586df35ed3bee75664063eb5bcf3fc9a59
geekskick <geeks_kick@yahoo.co.uk>
2022-12-16 11:13:32 +0000

    Things seem to be working

--- src/index.js
+++ src/index.js
@@ -14,6 +14,59 @@ import { SnackbarProvider, useSnackbar } from 'notistack';
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography';
 import ResultCard from './result_card';
+import Drawer from '@mui/material/Drawer'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import Toolbar from '@mui/material/Toolbar';
+import { CssBaseline } from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import InfoIcon from '@mui/icons-material/Info';
+const drawerWidth = 240;
+function Sidebar(props) {
+    const [aboutOpen, setAboutOpen] = React.useState(false);
+
+    /**
+         * The about window uses a single state variable to indicate if it's open or not.
+         * This sets that variable to true, thus opening the window on the next render
+         */
+    const handleAboutClick = () => {
+        setAboutOpen(true);
+    }
+
+    /**
+     * The about window uses a single state variable to indicate if it's open or not.
+     * This sets that variable to false, thus closing the window on the next render
+     */
+    const handleAboutClose = () => {
+        setAboutOpen(false);
+    }
+
+    return (<Drawer
+        sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+                width: drawerWidth,
+                boxSizing: 'border-box',
+            },
+        }}
+        variant="permanent"
+        anchor="left"
+    >
+        <Toolbar />
+        <List>
+            <ListItemButton onClick={handleAboutClick}>
+                <ListItemIcon>
+                    <InfoIcon />
+                </ListItemIcon>
+                <ListItemText primary="About" />
+            </ListItemButton>
+        </List>
+        <Divider />
+        <AboutDialog open={aboutOpen} handleAboutClose={handleAboutClose} />
+    </Drawer>);
+}
 
 function DismissButton(props) {
     return (<Button onClick={() => props.handleDismiss(props.key)}>Dismiss</Button>)
@@ -23,8 +76,6 @@ function App(props) {
     console.log("App props = ", props);
     const [selectedPokemon, setSelectedPokemon] = React.useState('');
     const [selectedGeneration, setSelectedGeneration] = React.useState('');
-    const [aboutOpen, setAboutOpen] = React.useState(false);
-    const [selectedAilment, setSelectedAilment] = React.useState('');
     const [captureProbability, setCaptureProbability] = React.useState({ possible: false, probability: 0.0 });
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
@@ -64,28 +115,8 @@ function App(props) {
         info(`Selected ${gen}`);
     }
 
-    const onStatusChange = (status) => {
-        info(`Selected ${status}`);
-        setSelectedAilment(status);
-    }
 
-    /**
-     * The about window uses a single state variable to indicate if it's open or not.
-     * This sets that variable to true, thus opening the window on the next render
-     */
-    const handleAboutClick = () => {
-        setAboutOpen(true);
-    }
-
-    /**
-     * The about window uses a single state variable to indicate if it's open or not.
-     * This sets that variable to false, thus closing the window on the next render
-     */
-    const handleAboutClose = () => {
-        setAboutOpen(false);
-    }
-
-    const allDataGathered = selectedGeneration !== '' && selectedPokemon !== '' && selectedAilment !== '';
+    const allDataGathered = selectedGeneration !== '' && selectedPokemon !== '';
 
     const probabilityData = (() => {
         if (captureProbability.possible) {
@@ -104,7 +135,6 @@ function App(props) {
                 <ResultCard
                     selectedPokemon={selectedPokemon}
                     selectedGeneration={selectedGeneration}
-                    selectedAilment={selectedAilment}
                     api={props.api}
                 />
             );
@@ -122,7 +152,7 @@ function App(props) {
                     That things are going to take up 4 of the 12 available columns - aka be 1/3 of the screen each
                 */}
                 <Grid container spacing={2}>
-                    <Grid item xs={4}>
+                    <Grid item xs={6}>
                         <Autocomplete
                             options={props.pokemonNames.map(key => { return { label: key } })}
                             isOptionEqualToValue={(left, right) => { return left.label === right.label }}
@@ -136,7 +166,7 @@ function App(props) {
                             renderInput={(params) => <TextField {...params} label="Select a Pokemon" />}
                         />
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={6}>
                         <Autocomplete
                             options={props.generations.map(gen => { return { label: gen } })}
                             isOptionEqualToValue={(left, right) => { return left.label === right.label }}
@@ -150,22 +180,6 @@ function App(props) {
                             renderInput={(params) => <TextField {...params} label="Select a Generation" />}
                         />
                     </Grid>
-                    <Grid item xs={4}>
-                        <Autocomplete
-                            options={props.ailments.map(option => { return { label: option } })}
-                            isOptionEqualToValue={(left, right) => {
-                                return left.label === right.label
-                            }}
-                            onChange={(event, value, reason) => {
-                                console.log(event, value, reason);
-                                if (reason === "selectOption") {
-                                    onStatusChange(value.label);
-                                }
-                            }}
-                            value={{ label: selectedAilment }}
-                            renderInput={(params) => <TextField {...params} label="Select a Status Ailment" />}
-                        />
-                    </Grid>
                 </Grid>
                 <hr className="rule" />
                 {probabilityData}
@@ -175,85 +189,55 @@ function App(props) {
     })();
 
     return (
-        <div>
-            <NavBar onAboutClick={handleAboutClick} />
-            <AboutDialog open={aboutOpen} handleAboutClose={handleAboutClose} />
-            <hr className="rule" />
-            {mainData}
-        </div >)
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <NavBar title={"Can I Catch It?"} width={`calc(100% - ${drawerWidth}px)`} />
+            <Sidebar />
+            {/*<AboutDialog open={aboutOpen} handleAboutClose={handleAboutClose} />*/}
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    bgcolor: 'background.default',
+                    p: 3
+                }}>
+                <Toolbar />
+                {mainData}
+            </Box>
+        </Box >)
 }
 
 function AppView() {
-    class DataToLoad {
-        static Generations = new DataToLoad('Generations');
-        static PokemonNames = new DataToLoad('PokemonNames');
-        static Ailments = new DataToLoad('Ailments');
-
-        constructor(name) {
-            this._name = name;
-        }
-
-        toString() {
-            return `DataToLoad.${this._name}`;
-        }
-    };
 
     const api = React.useRef(new Pokedex());
     const [generations, setGenerations] = React.useState([]);
-    const [ailments, setAilments] = React.useState([]);
     const [pokemonNames, setPokemonNames] = React.useState([]);
-    const dataRequests = React.useRef([]);
 
-    const isLoading = (data) => {
-        return dataRequests.current.some(item => data._name === item._name);
-    }
-
-    const startedLoading = (data) => {
-        dataRequests.current.push(data);
-    }
 
 
     React.useEffect(() => {
-        console.log("useEffect");
-        if (!isLoading(DataToLoad.Generations)) {
-            startedLoading(DataToLoad.Generations);
-            console.log("Requesting Generations");
-            api.current.getGenerationsList().then(generationsResults => {
-                console.log("generationsResults = ", generationsResults);
-                setGenerations(generationsResults.results.map(gen => gen.name));
-            });
 
-        }
+        console.log("Requesting Generations");
+        api.current.getGenerationsList().then(generationsResults => {
+            console.log("generationsResults = ", generationsResults);
+            setGenerations(generationsResults.results.map(gen => gen.name));
+        });
 
-        if (!isLoading(DataToLoad.Ailments)) {
-            startedLoading(DataToLoad.Ailments);
-            console.log("Requesting Ailments");
-            api.current.getMoveAilmentsList().then(ailmentsList => {
-                console.log("ailments = ", ailmentsList);
-                setAilments(ailmentsList.results.map(a => a.name));
-            });
-        }
+        api.current.getPokemonSpeciesList().then(speciesList => {
+            console.log("speciesList = ", speciesList);
+            setPokemonNames(speciesList.results.map(species => species.name));
+        });
 
-        if (!isLoading(DataToLoad.PokemonNames)) {
-            startedLoading(DataToLoad.PokemonNames);
-            console.log("Requesting PokemonNames");
-            api.current.getPokemonSpeciesList().then(speciesList => {
-                console.log("speciesList = ", speciesList);
-                setPokemonNames(speciesList.results.map(species => species.name));
-            });
-        }
 
-    }, [DataToLoad.Generations, DataToLoad.Ailments, DataToLoad.PokemonNames]);
+    }, []);
 
     console.log("pokemonNames = ", pokemonNames);
     console.log("generations = ", generations);
-    console.log("ailments = ", ailments);
     return (
         <SnackbarProvider maxSnack={3}>
             <App
                 pokemonNames={pokemonNames}
                 generations={generations}
-                ailments={ailments}
                 api={api.current}
             />
         </SnackbarProvider>);
--- src/navbar.js
+++ src/navbar.js
@@ -3,17 +3,23 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import './index.css'
+import { Typography } from '@mui/material';
+//import './index.css'
 export default function NavBar(props) {
-
+    console.log("NavBar props = ", props);
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Button color="inherit" onClick={props.onAboutClick}>About</Button>
-                </Toolbar>
-            </AppBar>
-        </Box >
+        <AppBar position="fixed"
+            sx={{
+                width: props.width,
+                ml: `${props.width}px`,
+                justifyContent: 'space-between'
+            }}>
+            <Toolbar>
+                <Typography variant="h6">
+                    {props.title}
+                </Typography>
+            </Toolbar>
+        </AppBar >
 
     );
 }
