32c34ff663e17ff6945708cebe59e69f3d712277
geekskick <geeks_kick@yahoo.co.uk>
2022-12-16 09:43:01 +0000

    balls are sort of on the page

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default
+    {
+        DEFAULT_SPRITE_URL: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+    };
--- src/result_card.js
+++ src/result_card.js
@@ -5,14 +5,83 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardHeader, Tooltip } from '@mui/material';
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import CONFIGURATION from './config.js';
+function BallOptions(props) {
+
+    const [ballList, setBallList] = React.useState([]);
+    const [speciesDetails, setSpeciesDetails] = React.useState({});
+
+    React.useEffect(() => {
+        console.log("BallOptions::useEffect");
+        props.api.getPokemonSpeciesByName(props.selectedPokemon).then(pokemon => {
+            console.log(pokemon);
+            setSpeciesDetails(pokemon);
+        }).catch(err => {
+            console.log(err);
+            setSpeciesDetails(null);
+        });
 
 
+        props.api.getItemCategoryByName('standard-balls').then(ballType => {
+            const balls = ballType.items.map(item => {
+                return item.name;
+            });
+            console.log(balls);
+            setBallList(balls);
+        }).catch(err => {
+            console.log(err);
+        });
+    }, [props.selectedPokemon]);
 
+    console.log("Rendering the BallOptions");
+    console.log("specied details = ", speciesDetails);
+    return (
+        <div>
+
+            {
+                ballList.map(ball => {
+                    // Make the icon and the text on the same line - pulled from here:
+                    // https://stackoverflow.com/questions/51940157/how-to-align-horizontal-icon-and-text-in-mui
+                    return (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                p: 1,
+                                m: 1,
+                                border: 'solid',
+                                bgcolor: 'background.paper',
+                                borderRadius: 1,
+                            }}>
+                            <Box sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                flexWrap: 'wrap',
+                            }}>
+                                <Box
+                                    component="img"
+                                    src={CONFIGURATION.DEFAULT_SPRITE_URL}
+                                />
+                                <Typography>{ball}</Typography>
+                            </Box>
+                            <Box>
+                                <Tooltip title={speciesDetails?.name}>
+                                    <Typography>{speciesDetails?.capture_rate}</Typography>
+                                </Tooltip>
+                            </Box>
+                        </Box >);
+                })
+            }
+        </div >);
+}
 
 export default function ResultCard(props) {
 
+    console.log("Result card props = ", props);
     const [pokemonDetails, setPokemonDetails] = React.useState();
-    const [speciesDetails, setSpeciesDetails] = React.useState();
 
     React.useEffect(() => {
         console.log("ResultCard::useEffect");
@@ -22,13 +91,6 @@ export default function ResultCard(props) {
         }).catch(err => {
             console.log(err);
             setPokemonDetails(null);
-        });
-
-        props.api.getPokemonSpeciesByName(props.selectedPokemon).then(pokemon => {
-            setSpeciesDetails(pokemon);
-        }).catch(err => {
-            console.log(err);
-            setSpeciesDetails(null);
         });
 
     }, [props.selectedPokemon]);
@@ -49,12 +111,14 @@ export default function ResultCard(props) {
         }
     }
     if (!sprite) {
-        sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+        sprite = CONFIGURATION.DEFAULT_SPRITE_URL;
         tooltip = `There are no sprite available for ${props.selectedPokemon}, so this is a default one`
     }
 
     return (<Card sx={{
-        width: '100', display: 'flex'
+        width: '100',
+        display: 'flex',
+        alignItems: 'center'
     }} >
         <CardHeader
             title={props.selectedPokemon}
@@ -70,9 +134,7 @@ export default function ResultCard(props) {
         </Tooltip>
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
             <CardContent sx={{ flex: '1 0 auto' }}>
-                <Typography>{props.selectedPokemon}</Typography>
-                <Typography>{props.selectedGeneration}</Typography>
-                <Typography>{props.selectedAilment}</Typography>
+                <BallOptions selectedGeneration={props.selectedGeneration} selectedPokemon={props.selectedPokemon} api={props.api} />
             </CardContent>
         </Box>
     </Card >);
