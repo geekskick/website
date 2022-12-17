a58760cb6b395c5a987d8929ded1f597f542d6dc
geekskick <geeks_kick@yahoo.co.uk>
2022-12-17 20:42:43 +0000

    hp slider looks nice

--- src/apps/can_i_catch_it/result_card.js
+++ src/apps/can_i_catch_it/result_card.js
@@ -8,7 +8,42 @@ import CardHeader from '@mui/material/CardHeader';
 import Tooltip from '@mui/material/Tooltip';
 import Slider from '@mui/material/Slider';
 import CONFIGURATION from './config.js';
+import { styled } from '@mui/material/styles';
+
 import calculateCaptureProbability from './calculations.js';
+
+const HPSlider = styled(Slider)(({ theme }) => ({
+    height: 2,
+    padding: '15px 0',
+    // The dragger
+    '& .MuiSlider-thumb': {
+        backgroundColor: 'blue',
+        //margin: '-18px 0 0',
+        borderRadius: '50%',
+        background: 'white',
+        cursor: 'pointer',
+        border: 0,
+    },
+    '& .MuiSlider-track': {
+        //opacity: 1,
+        backgroundColor: 'green',
+        border: 'solid',
+        borderColor: 'black',
+        height: '10px',
+        borderRadius: 0
+    },
+    '& .MuiSlider-rail': {
+        border: 'solid',
+        opacity: 1,
+        //width: '10px',
+        height: '10px',
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderRadius: 0
+    }
+}));
+
+
 function BallOptions(props) {
 
     const [ballList, setBallList] = React.useState([]);
@@ -158,7 +193,7 @@ export default function ResultCard(props) {
                     alt={props.selectedPokemon}
                     sx={{ width: 100 }}
                 />
-                <Slider
+                <HPSlider
                     defaultValue={hp}
                     min={1}
                     max={100}
