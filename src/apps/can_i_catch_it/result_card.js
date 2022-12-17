220e87d78c30a779454a563cc97a1a2e7b2d43b7
geekskick <geeks_kick@yahoo.co.uk>
2022-12-17 20:45:11 +0000

    hp slider refactored to own file

--- src/apps/can_i_catch_it/result_card.js
+++ src/apps/can_i_catch_it/result_card.js
@@ -6,42 +6,11 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardHeader from '@mui/material/CardHeader';
 import Tooltip from '@mui/material/Tooltip';
+import HPSlider from './hp_slider';
 import Slider from '@mui/material/Slider';
 import CONFIGURATION from './config.js';
-import { styled } from '@mui/material/styles';
 
 import calculateCaptureProbability from './calculations.js';
-
-const HPSlider = styled(Slider)(({ theme }) => ({
-    height: 2,
-    padding: '15px 0',
-    // The dragger
-    '& .MuiSlider-thumb': {
-        backgroundColor: 'blue',
-        //margin: '-18px 0 0',
-        borderRadius: '50%',
-        background: 'white',
-        cursor: 'pointer',
-        border: 0,
-    },
-    '& .MuiSlider-track': {
-        //opacity: 1,
-        backgroundColor: 'green',
-        border: 'solid',
-        borderColor: 'black',
-        height: '10px',
-        borderRadius: 0
-    },
-    '& .MuiSlider-rail': {
-        border: 'solid',
-        opacity: 1,
-        //width: '10px',
-        height: '10px',
-        backgroundColor: 'white',
-        borderColor: 'black',
-        borderRadius: 0
-    }
-}));
 
 
 function BallOptions(props) {
