fd2c3e1d5e4da8705a6a6f99ca964989ce19f101
geekskick <geeks_kick@yahoo.co.uk>
2023-09-30 07:49:03 +0000

    new itinerary

--- src/apps/roadmap/components/travel_icon.js
+++ src/apps/roadmap/components/travel_icon.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DriveEtaIcon from '@mui/icons-material/DriveEta';
 import FlightIcon from '@mui/icons-material/Flight';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 
 TravelIcon.propTypes = {
     method: PropTypes.string,
@@ -12,6 +13,8 @@ export default function TravelIcon(props) {
         return (<DriveEtaIcon />);
     } else if (method.toLowerCase() == 'flight') {
         return (<FlightIcon />);
+    } else if (method.toLowerCase() == 'boat') {
+        return (<DirectionsBoatIcon />);
     } else {
         return (<DriveEtaIcon />);
     }
